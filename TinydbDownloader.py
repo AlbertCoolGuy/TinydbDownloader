95b46579fcbd5d53cb295d198a74acea3a43f47f
AlbertCoolGuy <39652844+AlbertCoolGuy@users.noreply.github.com>
2020-04-23 14:51:02 +0200

    Update TinydbDownloader.py

--- TinydbDownloader.py
+++ TinydbDownloader.py
@@ -6,13 +6,15 @@ base_url = "https://tinydb.eiphax.tech/api"
 
 apps = requests.get(f"{base_url}/apps").text
 apps_json = json.loads(apps)
+
 def list_all_apps():
     for app in apps_json:
         if app["cia"]:
             id = app["id"]
             name = app["name"]
             headline = app["headline"]
-            print(f"ID {id}: {name} | {headline}")
+            last_updated = str(app["cia"][-1]["mtime"])[0:9]
+            print(f"ID {id}: {name} | {headline} | Last updated: {last_updated}")
         else:
             pass
 
@@ -29,27 +31,34 @@ def search_for_app(search):
         else:
             pass
    
-def option():   
-    choice = input("What do you want to do? (L)ist all apps or (S)earch for a app:")
-    if choice.lower() == "l":
-        list_all_apps()
-    elif choice.lower() == "s":
-        search_for_app(input("Enter a string to search for:"))
-    else:
-        print("Invalid choice, try again.")
-        option()
-        
+def option():
+    while True:   
+        choice = input("What do you want to do? (L)ist all apps, (S)earch for a app or (Q)uit the program:")
+        if choice.lower() == "l":
+            list_all_apps()
+            download()
+        elif choice.lower() == "s":
+            search_for_app(input("Enter a string to search for:"))
+            download()
+        elif choice.lower() == "q":
+            print("Quitting")
+            break
+        else:
+            print("Invalid choice, try again.")
 
 def download():
-    ids = input("Choose app id(s) to download in a comma separated list:").replace(" ", "").split(",")
-    for id in ids:
-        try:    
-            download_url = json.loads(requests.get(f"{base_url}/apps?app_id={id}").text)[0]["cia"][-1]["download_url"]
-            cia_file = requests.get(download_url)
-            open(download_url.split("/")[-1], "wb").write(cia_file.content)
-        except IndexError:
-            print("Invalid id, try again.")
-            download()
+    while True:
+        ids = input("Choose app id(s) to download in a comma separated list:").replace(" ", "").split(",")
+        for id in ids:
+            if id.isnumeric():
+                    try:
+                        download_url = json.loads(requests.get(f"{base_url}/apps?app_id={id}").text)[0]["cia"][-1]["download_url"]
+                        cia_file = requests.get(download_url)
+                        open(download_url.split("/")[-1], "wb").write(cia_file.content)
+                    except IndexError:
+                        print("Invalid id, try again.")
+            else:
+                    print("Input is not a number.")
             
 
 option()            
